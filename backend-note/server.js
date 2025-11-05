5062fbe4a5534e744bf86a3578c72f179a89e5ef
nithishanatchimuthu <nithishanatchimuthu28@gmail.com>
2025-11-05 11:36:46 +0530

    fix: update server configuration and dependencies

--- backend-note/server.js
+++ backend-note/server.js
@@ -1,44 +1,79 @@
 import express from "express";
 import cors from "cors";
-import { nanoid } from "nanoid";
+import pkg from "pg";
+
+const { Pool } = pkg;
 
 const app = express();
-const PORT = 5000; 
+const PORT = 5000;
 
-app.use(cors()); 
- 
-let notes = [];
+app.use(cors());
+app.use(express.json());
 
-app.get("/notes", (req, res) => {
-  res.json(notes);
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "notes_app",
+  password: "12345678", 
+  port: 5432,
+
 });
 
-app.post("/notes", (req, res) => {
-  const { text } = req.body;
 
-  if (!text) {
-    return res.status(400).json({ message: "Text is required" });
+
+app.get("/notes", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM notes ORDER BY id ASC");
+    res.json(result.rows);
+  }
+   catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error fetching notes" });
+  }
+});
+
+
+
+
+app.post("/notes", async (req, res) => {
+  const { text } = req.body;
+  if (!text) return res.status(400).json({ message: "Text is required" });
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO notes (text) VALUES ($1) RETURNING *",
+      [text]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } 
+  catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error adding note" });
   }
 
-  const date = new Date();
-  const newNote = {
-    id: nanoid(),
-    text,
-    date: date.toLocaleDateString(),
-  };
-  notes.push(newNote);
-  res.status(201).json(newNote);
 });
 
-app.delete("/notes/:id", (req, res) => {
+app.delete("/notes/:id", async (req, res) => {
   const { id } = req.params;
-  notes = notes.filter((note) => note.id !== id);
-  res.json({ message: "Note deleted successfully" });
-}
-);
-app.listen(5000, () => { 
+  try {
+    await pool.query("DELETE FROM notes WHERE id = $1", [id]);
+
+    res.json({ message: "Note deleted successfully" }
+
+    );
+  } 
+  catch (err) {
+    console.error(err);
+
+    res.status(500).json({ message: "Error deleting note" });
+  }
+});
+
+
+app.listen(5000, () => {
+
   console.log('Server running on localhost:5000');
 }
 );
-
-
